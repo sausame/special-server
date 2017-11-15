cf12e70745db766f7835b94e2aae38d2d4230787
sausame <bi_song@163.com>
2017-11-15 18:54:41 +0800

    Add searcher

---
+++ search/search.php
@@ -0,0 +1,35 @@
+<?php
+
+if (! empty($_POST)) {
+	$content = $_POST['content'];
+} elseif (! empty($_GET)) {
+	$content = $_GET['content'];
+} else {
+	die('NO content');
+}
+
+if (empty($content)) {
+	die('NO content');
+}
+
+$config = parse_ini_file('../config.ini');
+
+$configFile = $config['searcher-config-path'];
+$scriptFile = $config['searcher-script-path'];
+
+$tempFile = tempnam(sys_get_temp_dir(), 'search-result-');
+
+$cmd = 'bash ' . $scriptFile . ' ' . $configFile . ' "' . $content . '" ' . $tempFile;
+
+$output = system($cmd, $retval);
+
+if (0 === $retval) {
+	readfile($tempFile);
+} else {
+	die('Error in searching');
+}
+
+unlink($tempFile);
+
+?>
+
