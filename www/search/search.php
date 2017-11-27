5666f792c4955ed2406d70395dff14cc348f5bfd
sausame <bi_song@163.com>
2017-11-27 17:41:15 +0800

    Add safety level for searching

--- www/search/search.php
+++ www/search/search.php
@@ -1,39 +1,9 @@
 <?php
 
-if (! empty($_POST)) {
-	$content = $_POST['content'];
-} elseif (! empty($_GET)) {
-	$content = $_GET['content'];
-} else {
-	die('NO content');
-}
+include_once('manager.php');
 
-$content = trim($content);
+$manager = new SearchManager('../../config.ini');
 
-if (empty($content)) {
-	die('NO content');
-}
-
-$content = urlencode($content);
-
-$config = parse_ini_file('../../config.ini');
-
-$configFile = $config['searcher-config-path'];
-$scriptFile = $config['searcher-script-path'];
-
-$tempFile = tempnam(sys_get_temp_dir(), 'search-result-');
-
-$cmd = '/bin/bash ' . $scriptFile . ' ' . $configFile . ' "' . $content . '" ' . $tempFile;
-
-$output = system($cmd, $retval);
-
-if (0 === $retval) {
-	readfile($tempFile);
-} else {
-	die('Error in searching');
-}
-
-unlink($tempFile);
+echo($manager->run());
 
 ?>
-
