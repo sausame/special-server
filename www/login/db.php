814e2b18dc7abe0ff9eab66fea02396fdb904ea0
sausame <bi_song@163.com>
2017-12-05 12:14:56 +0800

    Support db config from config files

--- www/login/db.php
+++ www/login/db.php
@@ -1,9 +1,16 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","register");
+chdir(dirname(__FILE__));
+$config = parse_ini_file('../../config.ini');
+
+$host = $config["mysql-host"];
+$username = $config["mysql-user"];
+$password = $config["mysql-password"];
+$dbname = $config["db-name"];
+
+$con = mysqli_connect($host, $username, $password, $dbname);
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
 ?>
