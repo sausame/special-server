c26fb7a76505f81cc48f5feccb87a1bec917cdc2
sausame <bi_song@163.com>
2018-01-06 19:00:35 +0800

    Save user id into memory

--- www/view/auth.php
+++ www/view/auth.php
@@ -7,13 +7,16 @@ if (isset($_COOKIE['ID_your_site'])) { //if there is, it logs you in and directe
  	$username = $_COOKIE['ID_your_site']; 
  	$password = $_COOKIE['Key_your_site'];
 
-	$query = "SELECT * FROM `users` WHERE username = '$username' and password = '$password'";
+	$query = "SELECT id FROM `users` WHERE username = '$username' and password = '$password'";
 	$result = mysqli_query($con, $query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
 	if(0 == $rows) {
 		header("Location: ../login/login.php");
 		exit();
 	}
+
+	$row = mysqli_fetch_row($result);
+	$userId = $row[0];
 } else {
 	header("Location: ../login/login.php");
 	exit();
